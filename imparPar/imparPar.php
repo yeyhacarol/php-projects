dd1fc5ee5e866e735130e07f536896a67928ca80
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-02-10 16:51:23 -0300

    tentando sem sucesso!

--- imparPar/imparPar.php
+++ imparPar/imparPar.php
@@ -1,7 +1,17 @@
 <?php
 
-$impar = (int) 0;
-$par = (int) 0;
+$sltUm = range(0, 500);
+$sltDois = range(100, 1000);
+$inicial = (int) 0;
+$final = (int) 0;
+
+if (isset($_POST['btnCalcular'])) {
+
+    $inicial = $_POST[$valueItem];
+
+    
+
+}
 
 ?>
 
@@ -9,17 +19,32 @@ $par = (int) 0;
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form name="frmImparPar" method="$_POST" action="imparPar.php">
-        <select name="SltImpar">
-            
-        </select>   
+        <select name="sltUm">
+            <?php
+            foreach ($sltUm as $keyItem => $valueItem) {
+                echo "<option> $valueItem </option>";
+            }
+            ?>
+        </select>
+        <select name="sltDois">
+            <?php
+                foreach ($sltDois as $keyItem => $valueItem) {
+                    echo "<option> $valueItem </option>";
+                }
+            ?>
+        </select>
+        <input type="submit" name="btnCalcular" value="Calcular"></input>
     </form>
 </body>
+
 </html>
